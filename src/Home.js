8f23436c077cfc9be082e3cf1e514c0067950cc9
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-16 14:34:40 +0530

    Home added

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,20 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='homeContainer'>
+        <img className='homeImage' src="https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/apparelGW/U599/JUNE/PC/2._CB667373073_.jpg" alt=" "/>
+      <div className='homeRow'>
+
+      </div>
+      <div className='homeRow'>
+        
+      </div>
+      <div className='homeRow'>
+        
+      </div>
+      </div>
+    </div>
   )
 }
 
