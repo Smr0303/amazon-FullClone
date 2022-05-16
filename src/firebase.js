dd73f5f4ec952ef00c4ce988315e81e6bf9a6932
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-16 11:33:44 +0530

    started

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-po9zZGNWka37t1d71mRDD5n6WfdiR1Y",
+    authDomain: "fullclone-70887.firebaseapp.com",
+    projectId: "fullclone-70887",
+    storageBucket: "fullclone-70887.appspot.com",
+    messagingSenderId: "391445635272",
+    appId: "1:391445635272:web:5724c0a7474b63308e05fd",
+    measurementId: "G-LV01JDJVWQ"
+  };
