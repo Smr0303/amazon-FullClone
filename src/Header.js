f260dd971763ca9f87698986a593903301091a6a
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-16 12:45:14 +0530

    Header complete

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,44 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
         <img className='headerLogo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt='logo'/>
-        <div className='header_search'>
+        <div className='headerSearch'>
             <input className='headerInput' type="text"/>
-            {/* Logo */}
+            <SearchIcon className='headerSearchIcon'    />
         </div>
         <div className='headerNav'>
             <div className='headerOption'>
-                <span className='headerOptionLineOn'>
+                <span className='headerOptionLineOne'>
                 Hello Guest
                 </span>
-                <span className='headerOptionTw'>
+                <span className='headerOptionTwo'>
                 Sign In
                 </span>
             </div>
             <div className='headerOption'>
-                <span className='headerOptionLineOn'>
+                <span className='headerOptionLineOne'>
                  Return
                 </span>
-                <span className='headerOptionTw'>
+                <span className='headerOptionTwo'>
                 & Orders 
                 </span>
             </div>
             <div className='headerOption'>
-                <span className='headerOptionLineOn'>
+                <span className='headerOptionLineOne'>
                 Your
                 </span>
-                <span className='headerOptionTw'>
+                <span className='headerOptionTwo'>
                   Prime  
                 </span>
+            </div>
+            <div className='headerOptionBasket'>
+                <ShoppingBasketIcon/>
+                <div className='headerOptionTwo headerBasketCount'>0</div>
             </div>
         </div>
     
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-ReactDOM.render(
+import {createRoot} from 'react-dom/client';
+const container=document.getElementById('root');
+const root=createRoot(container);
+root.render(
   <App />,
-  document.getElementById('root')
+
 );
