76f4c4dbff002aad16199c2606d5980f196cf024
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-16 23:50:26 +0530

    Reducer added successfuly

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import './Product.css';
-
+import { useStateValue } from './StateProvider';
 function Product({id,title,image,price,rating}){
+    const [{basket},dispatch]=useStateValue();
+
+    console.log("Heyyy your cart",basket);
+const addToBasket=()=>{
+dispatch({
+    type:"ADD_TO_BASKET",
+    item:{
+        id:id,
+        title:title,
+        image:image,
+        price:price,
+        rating:rating,
+    },
+});
+}
+
     return(
         <div className='product'>
          <p className='productInfo'>{title}</p>
@@ -14,7 +30,7 @@ function Product({id,title,image,price,rating}){
              
              </div>
          <img className='productImage' src={image} alt=""/>
-        <button>Add to basket</button>
+        <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import {createRoot} from 'react-dom/client';
-const container=document.getElementById('root');
-const root=createRoot(container);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./reducer";
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
-  <App />,
-
+ 
+  <StateProvider initialState={initialState} reducer={reducer}>
+    <App />,
+  </StateProvider>
 );
