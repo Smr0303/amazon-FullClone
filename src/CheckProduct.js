35d6a6ef2a7f682f7edfb458e7b5242317058ed4
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-17 00:40:17 +0530

    Added products for checkout

--- src/CheckProduct.js
+++ src/CheckProduct.js
@@ -1,9 +1,46 @@
 import React from 'react'
-import './CheckoutProduct.css'
+import './CheckProduct.css'
+import { useStateValue } from './StateProvider';
+import CurrencyFormat from 'react-currency-format';
 
-function CheckProduct() {
+function CheckProduct({id,title,image,price,rating}) {
+    const [{basket},dispatch]=useStateValue();
+
   return (
-    <div>CheckProduct</div>
+    <div className='checkout_product'>
+            <img className='checkout_product_image' src={image} alt='' />
+
+            <CurrencyFormat renderText={(value) => (
+                <>
+                    <div className='checkout_product_info'>
+
+                        <p className='checkout_product_title'>{title}</p>
+
+                        <p className='checkout_product_price'>
+                            <strong>{value}</strong>
+                        </p>
+
+                        <div className='checkout_product_rating'>
+                            {Array(rating).fill().map((_, i) => (
+                                <p key={i}> ⭐ </p>
+                            ))}
+                        </div>
+                        
+                    <button>Remove from Basket</button>
+                        
+
+                    </div>
+                </>
+            )}
+
+                decimalScale={2}
+                value={price}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
+
+        </div>
   )
 }
 
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,25 +1,40 @@
-import React from 'react'
-import './Checkout.css';
-import Subtotal from './Subtotal'
-import CheckProduct from './CheckProduct';
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+import CheckProduct from "./CheckProduct";
+import { useStateValue } from "./StateProvider";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
-    <div className='checkout'>
-    <div className='checkoutLeft'>
-    <img className='checkoutAd' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
-   
-    <div>
-    <h2 className='checkoutTitle'>You subtitle here</h2>
-    </div>
-    </div>
-    <div className='checkoutRight'>
-     <Subtotal/>
+    <div className="checkout">
+      <div className="checkoutLeft">
+        <img
+          className="checkoutAd"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
 
+        <div>
+          <h2 className="checkoutTitle">You subtitle here</h2>
+          {basket.map((ele,i) => {
+            return (
+              <CheckProduct key={i}
+                id={ele.id}
+                title={ele.title}
+                price={ele.price}
+                image={ele.image}
+                rating={ele.rating}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="checkoutRight">
+        <Subtotal />
+      </div>
     </div>
-        
-    </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
