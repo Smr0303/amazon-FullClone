0538df3267232c94fc2f8bc9eae47a167c67a41d
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-16 22:40:43 +0530

    Subtotal started

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Checkout.css';
+import Subtotal from './Subtotal'
 
 function Checkout() {
   return (
     <div className='checkout'>
+    <div className='checkoutLeft'>
+    <img className='checkoutAd' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
+   
+    <div>
+    <h2 className='checkoutTitle'>You subtitle here</h2>
+    </div>
+    </div>
+    <div className='checkoutRight'>
+     <h2> the subtotal will go here</h2>
+     <Subtotal/>
 
+    </div>
         
     </div>
   )
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
+        <Link to="/">
+        
         <img className='headerLogo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt='logo'/>
+        </Link>
         <div className='headerSearch'>
             <input className='headerInput' type="text"/>
             <SearchIcon className='headerSearchIcon'    />
@@ -36,10 +40,12 @@ function Header() {
                   Prime  
                 </span>
             </div>
+            <Link to="/checkout">
             <div className='headerOptionBasket'>
                 <ShoppingBasketIcon/>
                 <div className='headerOptionTwo headerBasketCount'>0</div>
             </div>
+            </Link>
         </div>
     
     
