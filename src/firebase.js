578d79b2db31848b4c6368c8945c36140ca3b751
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-17 10:54:33 +0530

    firebase store setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyA-po9zZGNWka37t1d71mRDD5n6WfdiR1Y",
     authDomain: "fullclone-70887.firebaseapp.com",
@@ -8,3 +12,9 @@ const firebaseConfig = {
     appId: "1:391445635272:web:5724c0a7474b63308e05fd",
     measurementId: "G-LV01JDJVWQ"
   };
+
+  const firebaseApp=firebase.InitializeApp(firebaseConfig);
+  const db=firebaseApp.firestore();
+  const auth=firebase.auth();
+
+  export {db,auth};
