384641d9444e06cba2bc7d8487a56c5a826b95a5
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-18 13:47:29 +0530

    stripe integration started

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Payment.css";
+import CheckProduct from "./CheckProduct";
 
+import { useStateValue } from "./StateProvider";
 function Payment() {
+  const [{ basket, user }] = useStateValue();
   return (
     <div className="payment">
       <div className="paymentContainer">
@@ -9,9 +12,36 @@ function Payment() {
           <div className="paymentTitle">
             <h3>Delievery Address </h3>
           </div>
+          <div className="paymentAddress">
+            <p>{user?user.email:"Hello Guest@amazon.com"}</p>
+            <p>Kamploos colony</p>
+            <p>Xostring Mars</p>
+          </div>
         </div>
-        <div className="paymentSection"></div>
-        <div className="paymentSection"></div>
+        <div className="paymentSection">
+          <div className="paymentTitle">
+            <h3>Review Items and Delievery </h3>
+          </div>
+          <div className="paymentItems"> 
+          {basket.map((ele,i)=>{
+            return(
+              <CheckProduct key={i}
+              id={ele.id}
+              title={ele.title}
+              price={ele.price}
+              image={ele.image}
+              rating={ele.rating}/>
+            )
+          })}
+          </div>
+        </div>
+        <div className="paymentSection">
+        <div className="paymentTitle">
+            <h3>Payment Method </h3>
+          </div>
+          <div className=''></div>
+
+        </div>
       </div>
     </div>
   );
