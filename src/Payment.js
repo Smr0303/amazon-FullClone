4a5bd8e4d582ac62e36b288a241975b0c20f94e1
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-18 13:25:25 +0530

    Payment.js done

--- src/Payment.js
+++ src/Payment.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './Payment.css'
+import React from "react";
+import "./Payment.css";
 
 function Payment() {
   return (
-    <div className='payment'>Payment</div>
-  )
+    <div className="payment">
+      <div className="paymentContainer">
+        <div className="paymentSection">
+          <div className="paymentTitle">
+            <h3>Delievery Address </h3>
+          </div>
+        </div>
+        <div className="paymentSection"></div>
+        <div className="paymentSection"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Payment
+export default Payment;
