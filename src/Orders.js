383c8b899d3576143704da07a8bd1005529eedd6
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-19 14:55:41 +0530

    Orders page in process

--- src/Orders.js
+++ src/Orders.js
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import "./Orders.css";
 function Orders() {
+  const [orders, setOrders] = useState();
+  const [{ user }] = useStateValue();
+
+  useEffect(() => {
+    db.collection("users")
+      .doc(user?.uid)
+      .collection("orders")
+      .orderBy("created", "desc")
+      .onSnapshot((snapshot) => {
+        setOrders(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+
   return (
-    <div>Orders</div>
-  )
+    <div className="orders">
+      <h2>Orders</h2>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
